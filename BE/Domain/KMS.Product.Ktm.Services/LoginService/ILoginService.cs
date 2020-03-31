a51cd31c75a750ee07a5abf86dbb84a3171511c8
hoanghta <hoanghta@kms-technology.com>
2020-03-31 09:37:02 +0700

    [KTMS 12-13] Sync Teams and Members from AD - Add login models - Add login services

---
+++ BE/Domain/KMS.Product.Ktm.Services/LoginService/ILoginService.cs
@@ -0,0 +1,31 @@
+﻿using KMS.Product.Ktm.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMS.Product.Ktm.Services.LoginService
+{
+    public interface ILoginService
+    {
+        /// <summary>
+        /// Login with configuration from appsettings
+        /// </summary>
+        /// <returns></returns>
+        Task<string> LoginWithConfiguration();
+
+
+        /// <summary>
+        /// Login with UserLogin parameters
+        /// </summary>
+        /// <returns></returns>
+        Task<string> LoginWithParameters(UserLogin userLogin);
+
+        /// <summary>
+        /// Login with token from appsettings
+        /// This is used for temporary testing only
+        /// </summary>
+        /// <returns></returns>
+        string LoginWithTokens();
+    }
+}
