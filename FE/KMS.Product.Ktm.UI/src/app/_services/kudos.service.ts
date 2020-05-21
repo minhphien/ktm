6c74c0e5792fc0ad66a7786bf4872e98f483fe50
Phien Le <phienle@kms-technology.com>
2020-05-21 08:34:33 +0700

    refactor params for kudos service p.1

--- FE/KMS.Product.Ktm.UI/src/app/_services/kudos.service.ts
+++ FE/KMS.Product.Ktm.UI/src/app/_services/kudos.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable , of, throwError } from 'rxjs';
 import { retry, concat, retryWhen, take, flatMap, delay, map } from 'rxjs/operators';
 import { environment } from '@environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { LightKudos, KudosState, Kudos } from '@app/_models';
 import { Store } from '@ngrx/store';
 import { updateKudos } from '@app/_reducers/kudos-list.reducer';
@@ -99,13 +99,14 @@ export class KudosService {
   }
 
   getKudosAcrossTeamReportData(teamIds: string[], kudoType: number, dateRange?: Date[]): any {
-    let dateParams = `${dateRange && dateRange[0] ? '&dateFrom='+dateRange[0].toDateString():""}${dateRange && dateRange[1] ? '&dateTo='+dateRange[1].toDateString():""}`;
-    let teamIdsParams = _
-      .map(teamIds, x => "teamIds="+x)
-      .reduce((x,y) => x + "&" + y,"");
-    let url = `${environment.apiUrl}${environment.methods.ReportKudosAcrossTeam}?${teamIdsParams}&kudoTypeIds=${kudoType}${dateParams}`;
+    let params:HttpParams = new HttpParams()
+      .append("dateFrom", dateRange && dateRange[0] ? dateRange[0].toDateString(): "")
+      .append("dateTo", dateRange && dateRange[1] ? dateRange[1].toDateString(): "")
+      .append("kudoTypeIds", kudoType.toString())
+    _.each(teamIds, (x)=>{ params.append("teamIds",x)});
+    let url = `${environment.apiUrl}${environment.methods.ReportKudosAcrossTeam}`;
     console.log('requestKudos',url);
-    let request$ = this.http.get(url);
+    let request$ = this.http.get(url, {params: params});
     return request$;
   }
 }
